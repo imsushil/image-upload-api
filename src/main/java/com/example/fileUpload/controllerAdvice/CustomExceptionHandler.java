e9d46281bd3a4571428cf5c478c63e87d9172982
Sushil Yadav <sushil7161@gmail.com>
2019-11-20 00:22:20 +0530

    configurable origins allowed in CustomExceptionHandler

--- src/main/java/com/example/fileUpload/controllerAdvice/CustomExceptionHandler.java
+++ src/main/java/com/example/fileUpload/controllerAdvice/CustomExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.fileUpload.controllerAdvice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler  {
 	
+	@Value("#{'${allowed.origins}'.split(',')}")
+	List<String> allowedOrigins;
+	
 	@ExceptionHandler(MultipartException.class)
 	public ResponseEntity<String> handler(MultipartException e, HttpServletRequest req) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Access-Control-Allow-Origin", "http://localhost:4200");
+		if(this.allowedOrigins.size() > 1) {
+			headers.add("Access-Control-Allow-Origin", "*");
+		} else {
+			headers.add("Access-Control-Allow-Origin", this.allowedOrigins.get(0));
+		}
 		String message;
 		if(e.getMessage().contains("FileSizeLimitExceededException")) {
 			message = "Maximum upload size of file can be 1MB.";
